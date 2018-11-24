0760dfe7a833c98dcd07f237fb4e5a5be7544dad
Daniele Plantera <tango1266@arcor.de>
2018-11-24 12:20:33 +0100

    Added first rest endpoint posts

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,19 @@ const express = require('express');
 // (middleware - intersect client server communication)
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('First middleware');
-    // next avoids infinitive request
-    next();
-});
 
-app.use((req, res, next) => {
-    res.send('Hello from express!');
+app.use('/api/posts', (req, res, next) => {
+    const posts = [
+        {id:1, title:"First post", content:"First post content"},
+        {id:2, title:"Second post", content:"Second post content"},
+        {id:3, title:"Third post", content:"Third post content"},
+    ];
+
+    // will be returned
+    res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: posts
+    })
 });
 
 // export the app instance
