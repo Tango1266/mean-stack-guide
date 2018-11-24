2d6ac9ef80fa414005d4062d4217758383b9525f
Daniele Plantera <tango1266@arcor.de>
2018-11-24 12:36:32 +0100

    Wired frontend PostService with backend

--- backend/app.js
+++ backend/app.js
@@ -7,9 +7,9 @@ const app = express();
 
 app.use('/api/posts', (req, res, next) => {
     const posts = [
-        {id:1, title:"First post", content:"First post content"},
-        {id:2, title:"Second post", content:"Second post content"},
-        {id:3, title:"Third post", content:"Third post content"},
+        {id:"1", title:"First post", content:"First post content"},
+        {id:"2", title:"Second post", content:"Second post content"},
+        {id:"3", title:"Third post", content:"Third post content"},
     ];
 
     // will be returned
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -2,15 +2,30 @@ import {Injectable} from '@angular/core';
 import {Subject} from 'rxjs';
 
 import {Post} from './post.model';
+import {HttpClient} from '@angular/common/http';
+
+interface RespondObject {
+    message: string;
+    posts: Post[];
+}
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
     private posts: Post[] = [];
     private postsUpdated = new Subject<Post[]>();
 
+    constructor(private httpClient: HttpClient) {
+    }
 
     getPosts() {
-        return [...this.posts];
+        this.httpClient.get<RespondObject>('http://localhost:3000/api/posts')
+        // unsubscription will be handled in http framework
+            .subscribe(
+                // httpClient will return body of the respond
+                (postData) => {
+                    this.posts = postData.posts;
+                    this.postsUpdated.next([...this.posts]);
+                });
     }
 
     getPostUpdateListener(){
@@ -18,7 +33,7 @@ export class PostsService {
     }
 
     addPost(title: string, content: string) {
-        const post: Post = {title: title, content: content};
+        const post: Post = {id: null, title: title, content: content};
         this.posts.push(post);
         this.postsUpdated.next(this.getPosts());
     }
