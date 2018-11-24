8f7658e8a7929f00f77cd8f087ad605f194a4ec3
Daniele Plantera <tango1266@arcor.de>
2018-11-24 13:06:56 +0100

    Wired http call in posts.service

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -34,7 +34,12 @@ export class PostsService {
 
     addPost(title: string, content: string) {
         const post: Post = {id: null, title: title, content: content};
-        this.posts.push(post);
-        this.postsUpdated.next(this.getPosts());
+        this.httpClient.post<{ message: string }>('http://localhost:3000/api/posts', post)
+            .subscribe( (responseData) => {
+                // will only executed with successful request
+                console.log(responseData.message);
+                this.posts.push(post);
+                this.postsUpdated.next([...this.posts]);
+            });
     }
 }
