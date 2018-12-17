2e737d33bf87cc8a0872b3a9d5ddb4483132dfa6
Daniele Plantera <tango1266@arcor.de>
2018-12-17 22:57:06 +0100

    Added login endpoint (101)

--- backend/routs/user.js
+++ backend/routs/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -27,6 +28,51 @@ router.post('/signup', (req, res, err) => {
                 });
         });
 
+});
+
+router.post('/login', (req, res, next) => {
+    // user record from db
+    let fetchedUser;
+
+    // Find user by email
+    User.findOne({email: req.body.email})
+
+        // check weather password is valid
+        .then(user => {
+            if (!user){
+                return res.status(401).json({
+                    message: 'Auth failed!'
+                });
+            }
+            fetchedUser = user;
+
+            return bcrypt.compare(req.body.password, user.password);
+        })
+
+        // create token and respond it back
+        .then(result => {
+            if (!result){
+                return res.status(401).json({
+                    message: 'Auth failed!'
+                });
+            }
+
+            // create web token
+            const token = jwt.sign({email: fetchedUser.email, userId: fetchedUser._id},
+                'secret_string_this_should_be_longer', //todo:
+                {expiresIn: '1h'});
+
+            res.status(200).json({
+                token: token
+            });
+        })
+
+        // respond error if occur
+        .catch(err => {
+            res.status(401).json({
+                message: 'Auth failed!'
+            });
+        })
 });
 
 module.exports = router;
