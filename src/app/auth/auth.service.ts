0919e903e8b19f6ad3f1411b1e72e19adc88b889
Daniele Plantera <tango1266@arcor.de>
2018-12-18 13:08:14 +0100

    Added logout after token expires (110)

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -8,6 +8,7 @@ import {Router} from '@angular/router';
 @Injectable({providedIn: 'root'})
 export class AuthService {
     private token: string;
+    private tokenTimer: NodeJS.Timer;
     private authStatusListener = new Subject<boolean>();
     private isAuthenticated = false;
 
@@ -38,12 +39,18 @@ export class AuthService {
     login(email: string, password: string){
         const authData: AuthData = {email: email, password: password};
 
-        this.http.post<{token: string}>('http://localhost:3000/api/users/login', authData)
+        this.http.post<{token: string, expiresIn: number}>('http://localhost:3000/api/users/login', authData)
             .subscribe(response => {
                 const token = response.token;
                 this.token = token;
 
                 if (token) {
+                    const expiresInDuration = response.expiresIn;
+
+                    this.tokenTimer = setTimeout(() => {
+                        this.logout();
+                    }, expiresInDuration * 1000);
+
                     this.isAuthenticated = true;
                     this.authStatusListener.next(true);
                     this.router.navigate(['/']);
@@ -55,6 +62,7 @@ export class AuthService {
         this.token = null;
         this.isAuthenticated = false;
         this.authStatusListener.next(false);
+        clearTimeout(this.tokenTimer);
         this.router.navigate(['/']);
     }
 }
