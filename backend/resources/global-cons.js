f9c1bf5d18c928d4fa65e847eeb9ea3d6021aea2
Daniele Plantera <tango1266@arcor.de>
2018-12-18 10:58:17 +0100

    Refactored secret for auth in new module global-cons (102)

---
+++ backend/resources/global-cons.js
@@ -0,0 +1,5 @@
+const AUTH_SECRET = 'secret_string_this_should_be_longer';
+
+module.exports = AUTH_SECRET;
+
+
--- backend/routs/user.js
+++ backend/routs/user.js
@@ -3,8 +3,12 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+const globalCons = require("../resources/global-cons");
 
 const router = express.Router();
+
+// todo:
+const secretForAuth = globalCons.AUTH_SECRET;
 
 router.post('/signup', (req, res, err) => {
     bcrypt.hash(req.body.password, 10)
@@ -59,7 +63,7 @@ router.post('/login', (req, res, next) => {
 
             // create web token
             const token = jwt.sign({email: fetchedUser.email, userId: fetchedUser._id},
-                'secret_string_this_should_be_longer', //todo:
+                secretForAuth,
                 {expiresIn: '1h'});
 
             res.status(200).json({
