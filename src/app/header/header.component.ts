544243d6258cf2c2f442af8dbc6e663ac2592d97
Daniele Plantera <tango1266@arcor.de>
2018-12-18 12:30:10 +0100

    UI Header now reflects weather user are authenticated (105)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AuthService} from '../auth/auth.service';
+import {Subscription} from 'rxjs';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
+export class HeaderComponent implements OnInit, OnDestroy{
+    userIsAuthenticated = false;
 
-export class HeaderComponent {
+    private authListenerSubs: Subscription;
+
+    constructor(private authService: AuthService) {
+    }
+
+    ngOnInit(): void {
+        this.authListenerSubs = this.authService
+            .getAuthStatusListener()
+            .subscribe(isAuthenticated => {
+                this.userIsAuthenticated = isAuthenticated;
+            });
+    }
+
+    ngOnDestroy(): void {
+        this.authListenerSubs.unsubscribe();
+    }
 }
