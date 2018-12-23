e7f70a9fef0a6d134744ea88b7f6e501ffc7e1ea
Daniele Plantera <tango1266@arcor.de>
2018-12-23 08:53:16 +0100

    Extended db model 'post' with creator (user id) attribute (114)

--- backend/models/post.js
+++ backend/models/post.js
@@ -3,7 +3,16 @@ const mongoose = require('mongoose');
 const postSchema = mongoose.Schema({
     title: {type:String, required:true},
     content: {type:String,  required: true},
-    imagePath: {type:String, required: true}
+    imagePath: {type:String, required: true},
+
+    // user ID
+    creator: {
+        // creator is identified by ID managed by mongoose.
+        type: mongoose.Schema.Types.ObjectId,
+        // the ID 'creator' relates to the User Entity
+        ref: "User" ,
+        require: true
+    }
 });
 
 const postModel = mongoose.model('Post', postSchema);
